b487a1da467fc4431da164ed3409fe0838546fdb
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-11-01 03:37:11 +0500

    JWT Implemented

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Register new user
 router.post("/register", async (req, res) => {
@@ -16,6 +17,43 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Login logic
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+
+    !user && res.status(404).json({ message: "Wrong credentials" });
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET_PHRASE
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    originalPassword != req.body.password &&
+      res.status(401).json({ message: "Wrong credentials" });
+    console.log(originalPassword);
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
