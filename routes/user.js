6c1d096cbc45cecefcf84591c014819980d4dbbc
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-11-01 07:57:39 +0500

    User.PUT

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
-const { verifyToken } = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const { verifyTokenAndAuth } = require("./verifyToken");
+const User = require("../models/User");
 
-router.put("/:id", verifyToken, (req, res) => {
-  if (req.user.id === req.params.id) {
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_PHRASE
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,11 +3,13 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) {
         res.status(403).json({ message: "Token is not valid" });
       }
       req.user = user;
+      console.log(user);
       next();
     });
   } else {
@@ -15,4 +17,16 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-module.exports = { verifyToken };
+const verifyTokenAndAuth = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id == req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res
+        .status(403)
+        .json({ message: "You ar not allowed to take this action" });
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuth };
