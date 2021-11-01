ee0c4764ac1f2bd15f675627b6a143376aae6409
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-11-01 08:48:04 +0500

    Get all users

--- routes/user.js
+++ routes/user.js
@@ -46,4 +46,17 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
